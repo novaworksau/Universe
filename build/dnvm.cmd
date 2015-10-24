cdb4e6204011b40386f578575d780930869c05f6
BrennanConroy <brecon@microsoft.com>
2015-10-23 17:20:49 -0700

    Update dnvm.cmd

--- build/dnvm.cmd
+++ build/dnvm.cmd
@@ -1,6 +1,6 @@
 @Echo off
 
-set DNVM_CMD_PATH_FILE=PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.IO.Path]::GetTempFileName()"
+for /f "delims=" %%i in ('PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.IO.Path]::GetTempFileName()"') do set DNVM_CMD_PATH_FILE=%%i
 
 PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';$CmdPathFile='%DNVM_CMD_PATH_FILE%';& '%~dp0dnvm.ps1' %*"
 
